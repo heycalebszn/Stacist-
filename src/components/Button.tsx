f2c560fb7dbd6c616165cfdd11aa3906865a9010
Caleb Kalejaiye <kalejaiyecaleb@gmail.com>
2025-01-12 00:39:37 +0100

    navbar update

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,6 +2,6 @@ import { ButtonProps } from "../types/types"
 
 export const Button = ({ bgStyle }: ButtonProps) => {
     return(
-        <div className={`bg-${bgStyle}`}></div>
+        <div className={`bg-${bgStyle} w-[100px] rounded-lg text-white`}></div>
     )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,24 @@
+import { Logo } from "../assets"
 import { navLinks } from "../constants"
+import { Button } from "./Button"
 
 export const Navbar = () => {
     return(
-        <nav>
-            <ul>
+        <nav className="flex justify-between px-[50px] py-[30px] bg-transparent blur-lg">
+            <img src={Logo} alt="logo" />
+
+            <div className="flex text-white">
+            <ul className="flex gap-4">
             {navLinks.map((navLink, index) => (
-                <li key={index}><a href="#">{navLink.link}</a></li>
+                <li key={index}><a href="#" className="text-[0.7rem]">{navLink.link}</a></li>
             ))}
             </ul>
+            </div>
+
+            <div className="flex gap-4">
+                <Button bgStyle={"bg-transparent"} />
+                <Button bgStyle={"bg-gradient-to-b from-[#] to-[#]"} />
+            </div>
         </nav>
     )
 }
