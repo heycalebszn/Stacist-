8a8d0352980a2fc43a7ad909e29816f236dc4849
Caleb Kalejaiye <kalejaiyecaleb@gmail.com>
2025-01-15 00:45:44 +0100

    white menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,6 +7,9 @@ export const Navbar = () => {
     return(
         <nav className="flex justify-between px-[50px] py-[16px] bg-transparent bg-blur-lg items-center border-b border-[#161627] z-50 w-full">
             <img src={Logo} alt="logo" className="w-[80px]" />
+            <div className="flex md:hidden">
+            <RiMenu2Line className="text-[25px] text-white"/>
+            </div>
             <div className="md:flex text-white hidden">
             <ul className="flex gap-4">
             {navLinks.map((navLink, index) => (
@@ -20,10 +23,6 @@ export const Navbar = () => {
                 <div className="flex items-center justify-center rounded-[12px] px-[25px] py-[8px] bg-gradient-to-b from-[#01011F] to-[#111154]">
                     <Button bgStyle={"bg-transparent"} width={""} tag={"Sign up"} textSize="" />
                 </div>
-            </div>
-
-            <div className="flex md:hidden">
-            <RiMenu2Line className="text-[25px]"/>
             </div>
         </nav>
     )
