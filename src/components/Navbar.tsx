3b6ea787e777e3def7cd7d6459180f57f246db2a
Caleb Kalejaiye <kalejaiyecaleb@gmail.com>
2025-01-15 00:56:44 +0100

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,15 +2,21 @@ import { RiMenu2Line } from "react-icons/ri"
 import { Logo } from "../assets"
 import { navLinks } from "../constants"
 import { Button } from "./Button"
+import { useState } from "react"
 
 export const Navbar = () => {
+    const [openNav, setOpenNav] = useState(false);
+
+    const handleNavToggle = () => {
+        setOpenNav(true)
+    }
     return(
-        <nav className="flex justify-between px-[50px] py-[16px] bg-transparent bg-blur-lg items-center border-b border-[#161627] z-50 w-full">
-            <img src={Logo} alt="logo" className="w-[80px]" />
+        <nav className="flex justify-between px-[50px] py-[16px] bg-transparent bg-blur-lg items-center border-b border-[#161627] z-50 w-full relative">
+            <img src={Logo} alt="logo" className="md:w-[80px] w-[100px]" />
             <div className="flex md:hidden">
-            <RiMenu2Line className="text-[25px] text-white"/>
+            <RiMenu2Line className="text-[25px] text-white cursor pointer" onClick={handleNavToggle}/>
             </div>
-            <div className="md:flex text-white hidden">
+            <div className={`"md:flex text-white hidden" ${openNav ? "flex flex-col bg-gray-800 rounded-[12px] h-[150px] w-fit px-[100px] absolute top-0 right-0" : "hidden"}`}>
             <ul className="flex gap-4">
             {navLinks.map((navLink, index) => (
                 <li key={index}><a href="#" className="text-[0.8rem]">{navLink.link}</a></li>
